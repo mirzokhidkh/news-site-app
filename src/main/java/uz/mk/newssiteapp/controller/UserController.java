dc028522ec6e6362ed28432bb92f4fda44c3f1be
mirzokhidkhasanov <mirzohidkhasanov12@gmail.com>
2021-09-10 18:03:53 +0500

    config application.properties

--- src/main/java/uz/mk/newssiteapp/controller/UserController.java
+++ src/main/java/uz/mk/newssiteapp/controller/UserController.java
@@ -15,7 +15,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/User")
+@RequestMapping("/api/user")
 public class UserController {
 
     @Autowired
